60dc1f014e72f0c9c5aebf282e2c251acdbc657b
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-03-01 18:38:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(arr)
+  if arr.length == 0
+    puts "The line is currently empty."
+  else
+    people_in_line = String.new
+    arr.each_with_index{|name, position| people_in_line.concat(" #{position + 1}. #{name}")}
+    puts "The line is currently:#{people_in_line}"
+  end
+end# Write your code here.
+
+def take_a_number(arr, new_cust)
+  if arr.length == 0
+    arr.push(new_cust)
+    puts "Welcome, #{new_cust}. You are number #{arr.length} in line."
+  else
+    arr.push(new_cust)
+    puts "Welcome, #{new_cust}. You are number #{arr.length} in line."
+  end
+end
